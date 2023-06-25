fb6a9fe1b22874003ad0a8a2485aa48f29c6d33c
N. Justus <n-justus@outlook.com>
2023-06-25 13:26:09 +0200

    add missing dependencies

---
+++ frontend/bootstrap.js
@@ -0,0 +1,5 @@
+import './node_modules/bulma/css/bulma.min.css'
+import './node_modules/fontawesome-free/css/all.min.css'
+
+import './src/main/assets/scss/style.scss'
+import './target/scala-3.3.0/frontend-fastopt/main.js'
