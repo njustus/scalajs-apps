df05add0f001c18004300fdac84e1a3b03403812
N. Justus <n-justus@outlook.com>
2023-11-05 13:12:32 +0100

    include from scala-3.3.1 dir

--- frontend/bootstrap.js
+++ frontend/bootstrap.js
@@ -2,4 +2,4 @@ import './node_modules/bulma/css/bulma.min.css'
 import './node_modules/fontawesome-free/css/all.min.css'
 
 import './src/main/assets/scss/style.scss'
-import './target/scala-3.3.0/frontend-fastopt/main.js'
+import './target/scala-3.3.1/frontend-fastopt/main.js'
