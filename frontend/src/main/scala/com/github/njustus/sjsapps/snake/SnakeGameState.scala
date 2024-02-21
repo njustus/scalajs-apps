7822f2489fcd2602f90f4675faaa67eea1cdb2fc
N. Justus <n-justus@outlook.com>
2024-02-21 18:58:09 +0100

    use lenses

--- frontend/src/main/scala/com/github/njustus/sjsapps/snake/SnakeGameState.scala
+++ frontend/src/main/scala/com/github/njustus/sjsapps/snake/SnakeGameState.scala
@@ -7,6 +7,7 @@ import monocle.Lens
 import scala.concurrent.duration.Duration
 import scala.concurrent.duration.*
 import scala.language.postfixOps
+import monocle.macros.GenLens
 
 case class SnakeGameState(
                            board: Board,
@@ -14,28 +15,24 @@ case class SnakeGameState(
                          )
 
 object SnakeGameState {
-  //TODO use lens for updating state
+  private val snakeLens = GenLens[SnakeGameState](_.board.snake)
+  private val fruitLens = GenLens[SnakeGameState](_.board.fruit)
+
   //TODO detect fruit collision
   //TODO detect out-of-grid and begin at start
   def zero: SnakeGameState = SnakeGameState(Board.zero, KeyboardInputs.Right)
 
   def handleKeypress(ev: KeyboardInputs)(gs: SnakeGameState): SnakeGameState =
-    //TODO chain calls to moveSnake
     val delta = directionDelta(ev)
-    moveSnake(delta)(gs).copy(
-     snakeDirection = ev
-    )
+    moveSnake(delta)(gs).copy(snakeDirection = ev)
 
-  def moveSnake(gs: SnakeGameState): SnakeGameState =
-    moveSnake(directionDelta(gs.snakeDirection))(gs)
+  def moveSnake(gs: SnakeGameState): SnakeGameState = moveSnake(directionDelta(gs.snakeDirection))(gs)
 
-  private def moveSnake(delta: Coordinate)(gs: SnakeGameState): SnakeGameState =
-    val newSnake = (gs.board.snake.head |+| delta) :: gs.board.snake.init
-    val newBoard = gs.board.copy(
-      snake = newSnake
-    )
-    
-    gs.copy(board = newBoard)
+  private def moveSnake(delta: Coordinate): SnakeGameState => SnakeGameState =
+    snakeLens.modify { snake =>
+      val tail = snake.init
+      (snake.head |+| delta) :: tail
+    }
 
   private def directionDelta(input: KeyboardInputs): Coordinate = input match {
     case KeyboardInputs.Up    => Coordinate(0, -1)
