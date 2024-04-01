300b4787307d296c42a4c171877a64ec5ea3ebb5
N. Justus <n-justus@outlook.com>
2024-04-01 16:37:15 +0200

    use scalajs-router

--- frontend/src/main/scala/com/github/njustus/sjsapps/PageWrapper.scala
+++ frontend/src/main/scala/com/github/njustus/sjsapps/PageWrapper.scala
@@ -11,28 +11,53 @@ import com.github.njustus.sjsapps.snake.{SnakeGame, SnakeGameWrapper}
 import japgolly.scalajs.react.*
 import japgolly.scalajs.react.vdom.*
 import japgolly.scalajs.react.vdom.html_<^.*
+import japgolly.scalajs.react.extra.router.{given,*}
+import japgolly.scalajs.react.vdom.Implicits.* 
+import japgolly.scalajs.react.extra.router.Redirect
+import japgolly.scalajs.react.extra.router.Redirect
 
 object PageWrapper {
-  case class Props(pages: Map[String, PageComponent])
+  case class Page(name: String, component:PageComponent) {
+    val link = "/"+name.toLowerCase().replace(" ", "-")
+  }
+
+  case class Props(pages: List[Page]) {
+    require(!pages.isEmpty, "page list can not be empty")
+
+    private lazy val routerConfig = RouterConfigDsl[String].buildConfig { dsl =>
+      import dsl.*
+
+      val redirectHome = redirectToPage(pages.head.name)(SetRouteVia.HistoryPush)
+
+      val rules = pages.foldLeft(emptyRule) { (acc, page) =>
+        val route = staticRoute(page.link, page.name) ~> render(page.component())
+
+        println(s"adding route ${page.link}")
+
+        acc | route
+      }
+
+      rules.notFound(redirectHome)
+    }
+
+    lazy val router = Router(BaseUrl.fromWindowOrigin, routerConfig.logToConsole)
+  }
+
 
   case class State(selectedKey: String)
 
   private def renderFn(props: Props, state: Hooks.UseState[State]): VdomNode = {
-    val menuItems = props.pages.keys.toList.sorted.map { name =>
-      val className = if (state.value.selectedKey == name) "is-active" else ""
+    val menuItems = props.pages.map { page =>
 
-      def selectPage(name: String)(ev: ReactEvent) = {
-        ev.preventDefault()
-        state.modState(_.copy(selectedKey = name))
-      }
-
-      <.li(^.key := name, <.a(^.href := "#", ^.className := className, ^.onClick ==> selectPage(name), name))
+      <.li(^.key := page.name, <.a(^.href := page.link, page.name))
     }
+
+    val router = props.router
 
     <.div(
       ^.className := "column is-full columns",
       <.aside(^.className := "column menu", <.ul(^.className := "menu-list", menuItems.toVdomArray)),
-      <.div(^.className   := "column is-four-fifths", props.pages(state.value.selectedKey)())
+      <.div(^.className   := "column is-four-fifths", router())
     )
 
   }
@@ -44,16 +69,16 @@ object PageWrapper {
 
   def build() = {
     val p = Props(
-      Map(
-        "Tips Calculator"   -> TippsCalculator.component,
-        "Income Calculator" -> IncomeCalculator.component,
-        "Memory"            -> Memory.component,
-        "Form"              -> FormExample.component,
-        "Kanban"            -> BoardComponent.component,
-        "Complete 4"        -> Complete4Board.component,
-        "Capital Game"      -> GuessingGameWrapper.component,
-        "Snake Game"        -> SnakeGameWrapper.component,
-        "Notes"             -> NotesWrapper.component
+      List(
+        Page("Tips Calculator"   , TippsCalculator.component),
+        Page("Income Calculator" , IncomeCalculator.component),
+        Page("Memory"            , Memory.component),
+        Page("Form"              , FormExample.component),
+        Page("Kanban"            , BoardComponent.component),
+        Page("Complete 4"        , Complete4Board.component),
+        Page("Capital Game"      , GuessingGameWrapper.component),
+        Page("Snake Game"        , SnakeGameWrapper.component),
+        Page("Notes"             , NotesWrapper.component)
       )
     )
 
