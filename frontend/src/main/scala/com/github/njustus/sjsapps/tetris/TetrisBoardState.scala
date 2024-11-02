daf85fa40857aa40841d31f2d1e4b56da3845ed4
N. Justus <n-justus@outlook.com>
2024-11-02 23:11:21 +0100

    add pieces

--- frontend/src/main/scala/com/github/njustus/sjsapps/tetris/TetrisBoardState.scala
+++ frontend/src/main/scala/com/github/njustus/sjsapps/tetris/TetrisBoardState.scala
@@ -2,7 +2,7 @@ package com.github.njustus.sjsapps.tetris
 
 import com.github.njustus.sjsapps.shared.Coordinate
 import cats.syntax.semigroup.*
-import com.github.njustus.sjsapps.tetris.TetrisColor.Green
+import com.github.njustus.sjsapps.tetris.TetrisColor.{Green, Yellow}
 
 case class TetrisPiece(coordinates: List[Coordinate], color: TetrisColor) {
   def withDelta(c: Coordinate): TetrisPiece = this.copy(
@@ -58,19 +58,47 @@ object TetrisBoardState {
   }
 
   def tick(state: TetrisBoardState): TetrisBoardState = {
-    val piece = state.currentPiece.getOrElse(square(Green))
+    val piece = state.currentPiece.getOrElse(pieceT(Green))
       .withDelta(0 -> 1)
 
-    // TODO: out-of-range; collision check 
+    // TODO: out-of-range; collision check
     state.copy(currentPiece = Some(piece))
   }
 
   private def piece(coordinates: Coordinate*): TetrisColor => TetrisPiece = TetrisPiece(coordinates.toList, _)
 
-  val square: TetrisColor => TetrisPiece = piece(
+  val pieceO: TetrisColor => TetrisPiece = piece(
     (0, 0),
     (1, 0),
     (0, 1),
     (1, 1)
+  )
+
+  val pieceI: TetrisColor => TetrisPiece = piece(
+    (0,0),
+    (0,1),
+    (0,2),
+    (0,3),
+  )
+
+  val pieceL: TetrisColor => TetrisPiece = piece(
+    (0,0),
+    (0,1),
+    (0,2),
+    (1,2)
+  )
+
+  val pieceJ = piece (
+    (1,0),
+    (1,1),
+    (1,2),
+    (0,2)
+  )
+
+  val pieceT: TetrisColor => TetrisPiece = piece(
+    (0,0),
+    (1,0),
+    (2,0),
+    (1,1),
   )
 }
